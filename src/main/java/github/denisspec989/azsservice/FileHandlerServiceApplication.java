e365da379a7961900840bef9712e269301996135
Denis Ignatov <dsignatov@licard.com>
2023-02-27 09:07:27 +0300

    feat: rename service

--- src/main/java/github/denisspec989/azsservice/FileHandlerServiceApplication.java
+++ src/main/java/github/denisspec989/azsservice/FileHandlerServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AzsServiceApplication {
+public class FileHandlerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AzsServiceApplication.class, args);
+		SpringApplication.run(FileHandlerServiceApplication.class, args);
 	}
 
 }

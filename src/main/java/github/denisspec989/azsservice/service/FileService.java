3ab07cd9a2e510679186a0de0243278992c71465
Denis Ignatov <dsignatov@licard.com>
2023-02-27 07:44:00 +0300

    feat: gitignore + delete useless anotation

--- src/main/java/github/denisspec989/azsservice/service/FileService.java
+++ src/main/java/github/denisspec989/azsservice/service/FileService.java
@@ -4,7 +4,6 @@ import github.denisspec989.azsservice.domain.FileEntity;
 import github.denisspec989.azsservice.models.FileDto;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface FileService {
     FileEntity getFile(String fileName);
     void loadFileToDB(FileDto fileDto);

d4e516dd33c1128593f92f1d68de380448030569
Denis Ignatov <dsignatov@licard.com>
2023-03-04 21:07:19 +0300

    feat: enable discovery

--- src/main/java/github/denisspec989/azsservice/FileHandlerServiceApplication.java
+++ src/main/java/github/denisspec989/azsservice/FileHandlerServiceApplication.java
@@ -2,8 +2,10 @@ package github.denisspec989.azsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FileHandlerServiceApplication {
 
 	public static void main(String[] args) {
